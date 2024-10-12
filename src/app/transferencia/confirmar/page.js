6b40109aceff65cb400dde0d6b773c01dc092b65
mvnueloc <volguinc1700@alumno.ipn.mx>
2024-10-12 16:40:20 -0600

    Se agrego las vistas para realizar una transferecnia

---
+++ src/app/transferencia/confirmar/page.js
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+
+const Page = () => {
+  const name = "Tio Antonio";
+  const letter = "A";
+  const [monto, setMonto] = useState("100.00");
+  const concepto = "Carne asada.";
+  const fecha = "12 de Octubre de 2024";
+
+  const handleMontoChange = (e) => setMonto(e.target.value);
+
+  return (
+    <main className="h-100vh-8rem flex justify-center items-center">
+      <section className=" w-[90vw] bg-gray-100 py-6 rounded-lg">
+        <h2 className="font-semibold mx-6">Ingresa el monto a transferir</h2>
+
+        <div className="flex items-center  space-x-4 mt-6 mx-6">
+          {/* Letra de identificacion del destinatario*/}
+          <p className="bg-indigo-400 text-gray-200 w-12 h-12 rounded-full flex items-center justify-center">
+            {letter}
+          </p>
+
+          {/* Nombre del destinatario */}
+          <p className="font-light ">Para {name}</p>
+        </div>
+
+        <div className="text-center font-semibold text-6xl mt-20 mx-6 flex items-center justify-center">
+          <p>$ </p>
+          <input
+            className="w-full bg-gray-100  text-center"
+            type="number"
+            value={monto}
+            onChange={handleMontoChange}
+          />
+        </div>
+
+        {/* Boton para confirmar */}
+        <div className="text-center mt-20">
+          <Link
+            className=" bg-blue-500 text-gray-100 px-8 py-1 rounded-lg "
+            href={{
+              pathname: "./realizado",
+              query: {
+                name: name,
+                letter: letter,
+                monto: monto,
+                concepto: concepto,
+                fecha: fecha,
+              },
+            }}>
+            Continuar
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Page;
---
+++ src/app/transferencia/realizado/layout.js
@@ -0,0 +1,13 @@
+import Header from "../../components/common/header";
+
+export default function DashboardLayout({
+  children, // will be a page or nested layout
+}) {
+  return (
+    <section>
+      <Header />
+
+      {children}
+    </section>
+  );
+}
