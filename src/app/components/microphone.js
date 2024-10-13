03028ecfd4823084b9c9d114c4335ee929f89d48
mvnueloc <volguinc1700@alumno.ipn.mx>
2024-10-13 00:27:29 -0600

    Se agregaron las notificaciones y se hace la consulta al whisper con el boton del main

--- src/app/components/microphone.js
+++ src/app/components/microphone.js
@@ -1,32 +1,73 @@
 import Image from "next/image";
 import microphoneIcon from "/public/Microphone.svg";
 import microphoneComponent from "/public/Component-microphone.svg";
+import microphonePause from "/public/Component-pause.svg";
 import iconHome from "/public/Home.svg";
 import iconCard from "/public/Card.svg";
 
 import "../globals.css";
 
-export const MicrophoneContainer = ({ isActive, setIsActive}) => {
+// const StopIcon = () => (
+//   <svg
+//     className="text-white"
+//     xmlns="http://www.w3.org/2000/svg"
+//     width="24"
+//     height="24"
+//     viewBox="0 0 16 16">
+//     <path
+//       fill="currentColor"
+//       fillRule="evenodd"
+//       d="M4.5 1.5a3 3 0 0 0-3 3v7a3 3 0 0 0 3 3h7a3 3 0 0 0 3-3v-7a3 3 0 0 0-3-3z"
+//       clipRule="evenodd"
+//     />
+//   </svg>
+// );
 
+export const MicrophoneContainer = ({
+  isActive,
+  setIsActive,
+  handleBotonEscuchar,
+}) => {
   const toggleMicrophone = () => {
     setIsActive(!isActive);
+    handleBotonEscuchar();
   };
 
   return (
     <div className="container-microphone flex items-center justify-evenly w-full h-[85px] mt-[70px]">
-      <Image src={iconHome} alt="Microphone Icon" width={30} height={30} />
-      <div className="mb-20 container-microphone-img  rounded-[20rem]"
-      onClick={toggleMicrophone}
-      >
-        <Image
-          src={microphoneComponent}
-          alt="Microphone Icon"
-          width={100}
-          height={100}
-          className="m-3"
-        />
+      <Image
+        src={iconHome}
+        alt="Microphone Icon"
+        width={30}
+        height={30}
+      />
+      <div
+        className="mb-20 container-microphone-img  rounded-[20rem] cursor-pointer"
+        onClick={toggleMicrophone}>
+        {isActive ? (
+          <Image
+            src={microphonePause}
+            alt="Microphone Icon"
+            width={100}
+            height={100}
+            className="m-3"
+          />
+        ) : (
+          <Image
+            src={microphoneComponent}
+            alt="Microphone Icon"
+            width={100}
+            height={100}
+            className="m-3"
+          />
+        )}
       </div>
-      <Image src={iconCard} alt="Microphone Icon" width={30} height={30} />
+      <Image
+        src={iconCard}
+        alt="Microphone Icon"
+        width={30}
+        height={30}
+      />
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -7,27 +7,134 @@ import Avatar from "/public/avatar.png";
 import IconCardBank from "/public/iconcardtBank.png";
 import Image from "next/image";
 import "./globals.css";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [chat, setChat] = useState(null);
   const [messages, setMessages] = useState([]);
   const [recording, setRecording] = useState(false);
   const [isActive, setIsActive] = useState(false);
 
+  // <-- Notificaciones -->
+  useEffect(() => {
+    toast.warn(
+      `Modo seguro activado. Consulta a tu asistente para ver los cambios realizados.
+      `,
+      {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        theme: "light",
+      }
+    );
+  }, []);
+
+  // <-- Responder por voz
+
+  const mediaRecorderRef = useRef(null);
+  const audioChunksRef = useRef([]);
+
+  const startRecording = async () => {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      alert("Tu navegador no soporta la grabación de audio.");
+      return;
+    }
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaRecorderRef.current = new MediaRecorder(stream);
+      mediaRecorderRef.current.start();
+      setRecording(true);
+      audioChunksRef.current = [];
+
+      mediaRecorderRef.current.ondataavailable = (event) => {
+        audioChunksRef.current.push(event.data);
+      };
+
+      mediaRecorderRef.current.onstop = () => {
+        const audioBlob = new Blob(audioChunksRef.current, {
+          type: "audio/wav",
+        });
+        sendAudio(audioBlob);
+      };
+    } catch (error) {
+      console.error("Error al acceder al micrófono:", error);
+      alert("No se pudo acceder al micrófono.");
+    }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      setRecording(false);
+    }
+  };
+
+  const sendAudio = async (audioBlob) => {
+    const formData = new FormData();
+    formData.append("audio", audioBlob, "grabacion.wav");
+
+    try {
+      const response = await fetch("http://localhost:8080/transcribe", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Error en la transcripción");
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+      // alert(`Transcripción: ${data.text}`);
+      // sendMessage(data.text);
+      setInput("");
+      // console.log(data);
+      // console.log(data.text.action);
+    } catch (error) {
+      console.error("Error al transcribir el audio:", error);
+      alert("Hubo un error al transcribir el audio.");
+    }
+  };
+
+  const handleBotonEscuchar = () => {
+    if (recording) {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+
+    setRecording(!recording);
+  };
+
   useEffect(() => {
     if (isActive) {
+      console.log("Grabando");
     }
   }, [isActive]);
 
   return (
     <>
-      <div className="w-full h-full relative flex flex-col">
-        <Header name={"Gabriela"}/>
+      <ToastContainer />
+      <div className="w-full h-full relative flex flex-col ">
+        <Header name={"Gabriela"} />
 
-        <CardBank saldo="5300.15" />
-        <CardServices />
+        <div className={` ${isActive ? "blur" : "clase-inactiva"}`}>
+          <CardBank saldo="5300.15" />
+          <CardServices />
+        </div>
 
-        <div className="flex-grow">
+        <div className={`flex-grow ${isActive ? "blur" : "clase-inactiva"}`}>
           <div className="w-[90%] h-16 rounded-[1.2rem] overflow-hidden shadow-lg bg-white p-2 ml-4  mb-4 flex items-center justify-evenly relative">
             <Image
               src={IconCardBank}
@@ -43,7 +150,11 @@ export default function Home() {
           </div>
         </div>
 
-        <MicrophoneContainer isActive={isActive} setIsActive={setIsActive} />
+        <MicrophoneContainer
+          isActive={isActive}
+          setIsActive={setIsActive}
+          handleBotonEscuchar={handleBotonEscuchar}
+        />
       </div>
     </>
   );
