81cf269e5c5fb2e4eecbf84a64d977228a58853e
danielctecla <daniel.ctecla@gmail.com>
2024-10-13 10:47:07 -0600

    code fixed

--- src/app/transferencia/confirmar/page.js
+++ src/app/transferencia/confirmar/page.js
@@ -1,58 +1,34 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 const Page = () => {
   const searchParams = useSearchParams();
-  const router = useRouter();
-  
-  // Obtener los valores de los parámetros de búsqueda
-  const name = searchParams.get("name") || "Invitado"; // Valor predeterminado si 'name' es null
-  const letter = name.charAt(0).toUpperCase();
-  
-  let concepto_ = searchParams.get("concepto");
-  const concepto = concepto_ && concepto_ !== "undefined" ? concepto_ : "Sin concepto"; // Manejar el caso undefined
-  
-  let monto_ = searchParams.get("monto");
-  const [monto, setMonto] = useState(monto_ || 0); // Si monto es null, se usa 0 por defecto
-  
-  const fecha = new Date().toLocaleDateString(); // Obtener la fecha actual
 
-  // Puedes usar useEffect para manejar cualquier cambio en los parámetros
-  useEffect(() => {
-    setMonto(monto_ || 0); // Actualizar monto cuando el parámetro cambie
-  }, [monto_]);
+  const name = searchParams.get("name");
+  const letter = name.charAt(0).toUpperCase();
+  const monto = searchParams.get("monto");
+  const concepto = searchParams.get("concepto");
+  
+  const fecha = new Date().toLocaleDateString();
 
   const handleMontoChange = (e) => setMonto(e.target.value);
 
-  const handleContinue = () => {
-    router.push({
-      pathname: "./realizado",
-      query: {
-        name: name,
-        letter: letter,
-        monto: monto,
-        concepto: concepto,
-        fecha: fecha,
-      },
-    });
-  };
-
   return (
-    <main className="flex-grow justify-center items-center">
-      <section className=" w-full h-full bg-gray-100 py-6 rounded-lg">
-        <h2 className="text-center font-semibold mx-6">Ingresa el monto a transferir</h2>
+    <main className="h-100vh-8rem flex justify-center items-center">
+      <section className=" w-[90vw] bg-gray-100 py-6 rounded-lg">
+        <h2 className="font-semibold mx-6">Ingresa el monto a transferir</h2>
 
-        <div className="flex flex-col items-center mt-6 space-y-2">
+        <div className="flex items-center  space-x-4 mt-6 mx-6">
           {/* Letra de identificacion del destinatario*/}
           <p className="bg-indigo-400 text-gray-200 w-12 h-12 rounded-full flex items-center justify-center">
             {letter}
           </p>
 
           {/* Nombre del destinatario */}
-          <div className="font-normal ml-0">Para: </div>
-          <div className="text-center font-medium"> {name}</div>
+          <p className="font-light ">Para {name}</p>
         </div>
 
         <div className="text-center font-semibold text-6xl mt-20 mx-6 flex items-center justify-center">
@@ -67,12 +43,20 @@ const Page = () => {
 
         {/* Boton para confirmar */}
         <div className="text-center mt-20">
-          <button
-            className="bg-blue-500 text-gray-100 px-8 py-1 rounded-lg"
-            onClick={handleContinue}
-          >
+          <Link
+            className=" bg-blue-500 text-gray-100 px-8 py-1 rounded-lg "
+            href={{
+              pathname: "./realizado",
+              query: {
+                name: name,
+                letter: letter,
+                monto: monto,
+                concepto: concepto,
+                fecha: fecha,
+              },
+            }}>
             Continuar
-          </button>
+          </Link>
         </div>
       </section>
     </main>
